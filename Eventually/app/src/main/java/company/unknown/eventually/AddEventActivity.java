a8f8d713b7e6eab8bc71e146d1f45f1babdf959b
Lorenzo Martinico <lzmartinico@gmail.com>
2017-07-29 17:54:00 -0700

    Add empty LocationTracking service, schedule for 30 minutes before event

--- Eventually/app/src/main/java/company/unknown/eventually/AddEventActivity.java
+++ Eventually/app/src/main/java/company/unknown/eventually/AddEventActivity.java
@@ -1,14 +1,21 @@
 package company.unknown.eventually;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.Date;
+
 public class AddEventActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +29,17 @@ public class AddEventActivity extends AppCompatActivity {
 
     }
 
+    private PendingIntent pIntent;
+
     public void eventAdded(View view){
+        Context context = this;
+        AlarmManager manager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent timeTrackerIntent = new Intent(context, LocationTracking.class);
+        pIntent = PendingIntent.getBroadcast(context, 0, timeTrackerIntent, 0);
+
+        EventsEntity event = new EventsEntity("", "", null, new LocationsEntity("","",0f,0f)); //TODO: fetchEvent
+        manager.set(AlarmManager.RTC_WAKEUP, Long.parseLong(event.date) - 30*60*1000, pIntent);
+
         //TODO UPDATE COLLECTION
         Intent intent = new Intent(this, MainEventsActivity.class);
         startActivity(intent);
